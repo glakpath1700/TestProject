c895da7f0487c5400608d2cd7ddd85bd6006c5a2
glakpath1700 <glakpath@gmail.com>
2022-07-25 16:45:49 +0530

    this pom.xml added

---
+++ TestProject/src/com/lakpath/Test/SpringBoot/LakpathTest.java
@@ -0,0 +1,22 @@
+package com.lakpath.Test.SpringBoot;
+
+import java.io.Serializable;
+
+public class LakpathTest implements Runnable, Serializable {
+
+	public LakpathTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
